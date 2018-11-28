290bdc40e16b805a7bc8a5aff1ee0ceafedb53a5
seung0922 <42957486+seung0922@users.noreply.github.com>
2018-11-29 04:09:26 +0900

    Update test_11-29.py

--- test_11-29.py
+++ test_11-29.py
@@ -15,7 +15,8 @@ shopping.head()
 
 #######################################################
 
-# 성별로 구분
+## 성별로 구분
+
 # 남자만
 male = shopping[shopping['GENDER'] == 1]
 
@@ -24,20 +25,26 @@ female = shopping[shopping['GENDER'] == 2]
 
 #######################################################
 
+## 나이대별로 구분
+
 # 남자 나이대별로 그룹화
 M_age = male.groupby('AGE_PRD')
-
-# 남자 나이대별 총 구매액과 총구매액 높은 순서대로
-M_age['BUY_AM'].sum().sort_values(ascending=False)
 
 # 여자 나이대별로 그룹화
 F_age = female.groupby('AGE_PRD')
 
-# 남자 나이대별 총 구매액과 총구매액 높은 순서대로
-F_age['BUY_AM'].sum().sort_values(ascending=False)
-
 # 남자+여자 나이대별로 그룹화
 MF_age = shopping.groupby('AGE_PRD')
+
+#######################################################
+
+## 총구매금액
+
+# 남자 나이대별 총 구매액과 총구매액 높은 순서대로
+M_age['BUY_AM'].sum().sort_values(ascending=False)
+
+# 여자 나이대별 총 구매액과 총구매액 높은 순서대로
+F_age['BUY_AM'].sum().sort_values(ascending=False)
 
 # 남자+여자 나이대별 총 구매액과 총구매액 높은 순서대로
 MF_age['BUY_AM'].sum().sort_values(ascending=False)
@@ -45,8 +52,8 @@ MF_age['BUY_AM'].sum().sort_values(ascending=False)
 
 #######################################################
 
+## 많이 팔린 품목
 
-# 많이 팔린 품목 구하기
 # 모든 연령 남자 많이 팔린 품목
 d = dict()
 for i in male['PD_S_NM']:
@@ -62,4 +69,42 @@ for i in range(len(sort_d)):
     for j in range(2):
         print sort_d[i][j],
     print ''
+
+# 모든 연령 여자 많이 팔린 품목
+d = dict()
+for i in female['PD_S_NM']:
+    if i not in d:
+        d[i]=1
+    else:
+        d[i]=d[i]+1
+sort_d = sorted(d.items(), key = lambda t : t[1], reverse=True)
+
+print "<여자가 많이 산 품목>"
+for i in range(len(sort_d)):
+    print "{0}위 : ".format(i+1),
+    for j in range(2):
+        print sort_d[i][j],
+    print ''
     
+# 전체 많이 팔린 품목
+d = dict()
+for i in shopping['PD_S_NM']:
+    if i not in d:
+        d[i]=1
+    else:
+        d[i]=d[i]+1
+sort_d = sorted(d.items(), key = lambda t : t[1], reverse=True)
+
+print "<전체고객이 많이 산 품목>"
+for i in range(len(sort_d)):
+    print "{0}위 : ".format(i+1),
+    for j in range(2):
+        print sort_d[i][j],
+    print ''
+    
+# 나이대별 품목?
+    
+######################################################
+
+## 어느 업종에서 많이 팔렸나
+
